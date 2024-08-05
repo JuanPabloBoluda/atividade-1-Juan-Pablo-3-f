344b3b79935660394d655633b190742a0c03353b
JuanPabloBoluda <164278699+JuanPabloBoluda@users.noreply.github.com>
2024-08-05 22:52:02 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const meuNome = 'Juan';
+const idade = '18';
+const altura = '1.73';
+const massa = '62';
+const cidade = 'Sjp'
+
+
+console.log ("Meu nome é:", meuNome);
+console.log ("Minha idade é de:",idade);
+console.log ("Minha altura é de:",altura);
+console.log ("minha massa é de:", massa);
